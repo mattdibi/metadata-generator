3fc6ae16c489d1e342b0425fdd15b08130a32ba1
Mattia Dal Ben <matthewdibi@gmail.com>
2024-11-11 18:50:18 +0100

    feat: generate .classpath files

--- metadata-generator/__init__.py
+++ metadata-generator/__init__.py
@@ -38,6 +38,48 @@ def run():
     print(json.dumps(map, indent=4))
 
     #
+    # Generate .classpath file
+    #
+    for key,value in map.items():
+        if(not value["packaging"] == "eclipse-plugin" and not value["packaging"] == "eclipse-test-plugin"):
+            continue
+
+        module_path = value["path"]
+        libs = glob.glob(os.path.join(module_path, 'lib/*.jar'))
+
+        classpath = ET.Element('classpath')
+
+        classpathentry = ET.Element('classpathentry')
+        classpathentry.set('kind', 'con')
+        classpathentry.set('path', 'org.eclipse.jdt.launching.JRE_CONTAINER')
+        classpath.append(classpathentry)
+
+        classpathentry = ET.Element('classpathentry')
+        classpathentry.set('kind', 'con')
+        classpathentry.set('path', 'org.eclipse.pde.core.requiredPlugins')
+        classpath.append(classpathentry)
+
+        classpathentry = ET.Element('classpathentry')
+        classpathentry.set('kind', 'src')
+        classpathentry.set('path', 'src/main/java')
+        classpath.append(classpathentry)
+
+        for lib in libs:
+            classpathentry = ET.Element('classpathentry')
+            classpathentry.set('kind', 'lib')
+            classpathentry.set('path', os.path.join('libs', os.path.basename(lib)))
+            classpath.append(classpathentry)
+
+        classpathentry = ET.Element('classpathentry')
+        classpathentry.set('kind', 'output')
+        classpathentry.set('path', 'target/classes')
+        classpath.append(classpathentry)
+
+        tree = ET.ElementTree(classpath)
+        ET.indent(tree.getroot(), space="    ")
+        tree.write(os.path.join(value["path"], '.classpath'), encoding='utf-8', xml_declaration=True)
+
+    #
     # Generate javaConfig.json
     #
     target_platform = glob.glob('**/*.target', recursive=True)
